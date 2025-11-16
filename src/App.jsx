a05f706e46278bb60d1e02bb7fcc9a4970cbbbe6
Jorge Espinosa <jyer94@gmail.com>
2025-11-16 14:23:44 -0600

    build hash

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Story } from './pages/story/Story';
 export const App = () => {
 
     return (
-        <Router>
+        <Router basename={import.meta.env.VITE_PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Layout><HomePage /></Layout>} />
                 <Route path="/story/:id" element={<Layout><Story /></Layout>} />
