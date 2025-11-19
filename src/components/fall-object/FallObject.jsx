900743d4e1aa19a88672eb4edaaf9641a5251836
Jorge Espinosa <jyer94@gmail.com>
2025-11-18 23:27:34 -0600

    fix: imagen fallen object

--- src/components/fall-object/FallObject.jsx
+++ src/components/fall-object/FallObject.jsx
@@ -4,6 +4,7 @@ export const FallObject = ({ imgName, callbackClose }) => {
     return (
         <div className="fall-container" onClick={() => callbackClose()}>
             <img src={`../img/objects/${imgName}`} className="falling-object" />
+            {/* <img src={`../img/characters/jor.webp`} className="falling-object" /> */}
         </div>
     )
 }
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -5,7 +5,7 @@ export function useFirebase() {
 
     const writeDatafirebaseAsync = async (collectionName, data) => {
         try {
-            // const docRef = await addDoc(collection(db, collectionName), data);
+            const docRef = await addDoc(collection(db, collectionName), data);
             console.log('Documento creado con ID:', docRef.id);
         } catch (e) {
             console.error('Error agregando documento:', e);
