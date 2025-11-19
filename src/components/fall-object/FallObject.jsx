c09779ad9a355ad237a39a26e033490e480df7d3
Jorge Espinosa <jyer94@gmail.com>
2025-11-18 23:33:04 -0600

    feat: mesa madera en falling object

--- src/components/fall-object/FallObject.jsx
+++ src/components/fall-object/FallObject.jsx
@@ -2,7 +2,13 @@
 import './fall-object.scss';
 export const FallObject = ({ imgName, callbackClose }) => {
     return (
-        <div className="fall-container" onClick={() => callbackClose()}>
+        <div className="fall-container" onClick={() => callbackClose()}
+            style={{
+                backgroundImage: `url(../img/background/wood.webp)`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat'
+            }}
+        >
             <img src={`../img/objects/${imgName}`} className="falling-object" />
             {/* <img src={`../img/characters/jor.webp`} className="falling-object" /> */}
         </div>
