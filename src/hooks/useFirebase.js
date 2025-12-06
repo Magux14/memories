3124f6e6643be82233efb213b6af94ab0ce59e0f
Jorge Espinosa <jyer94@gmail.com>
2025-12-06 15:48:38 -0600

    feat: si es mi android no se loggea

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -4,11 +4,13 @@ import { db } from '../firebase';
 export function useFirebase() {
 
     const writeDatafirebaseAsync = async (collectionName, data) => {
-        try {
-            const docRef = await addDoc(collection(db, collectionName), data);
-            console.log('Documento creado con ID:', docRef.id);
-        } catch (e) {
-            console.error('Error agregando documento:', e);
+        if (data.deviceInfo != 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36') {
+            try {
+                const docRef = await addDoc(collection(db, collectionName), data);
+                console.log('Documento creado con ID:', docRef.id);
+            } catch (e) {
+                console.error('Error agregando documento:', e);
+            }
         }
     };
 
